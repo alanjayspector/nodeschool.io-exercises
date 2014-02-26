05e42b103a230fb2e4ba048c8558c37e9ad91aed
alanjayspector <me@aj5.org>
2014-02-26 00:36:10 -0800

    tried out vim jsbeautify plugin

--- combiner.js
+++ combiner.js
@@ -1,13 +1,13 @@
 (function() {
-  'use strict';
-  var combine = require('stream-combiner');
-  var split = require('split');
-  var through = require('through');
-  var zlib = require("zlib");
-  var _ = require("underscore");
+    'use strict';
+    var combine = require('stream-combiner');
+    var split = require('split');
+    var through = require('through');
+    var zlib = require("zlib");
+    var _ = require("underscore");
 
-  //zlib.createGzip()
-  /*  --- input
+    //zlib.createGzip()
+    /*  --- input
     {"type":"genre","name":"cyberpunk"}
     {"type":"book","name":"Neuromancer"}
     {"type":"book","name":"Snow Crash"}
@@ -16,36 +16,39 @@
     {"type":"book","name":"Void"}
   */
 
-  /* ----- output
+    /* ----- output
     {"name":"cyberpunk","books":["Neuromancer","Snow Crash"]}
     {"name":"space opera","books":["A Deepness in the SKy","Void"]}
   */
 
-  
 
-    module.exports = function () {
+
+    module.exports = function() {
         var genres = [];
         return combine(
             split(),
-            through( function(line) {
-                if ( line.length === 0 || _.isUndefined(line) ) return;
+            through(function(line) {
+                if (line.length === 0 || _.isUndefined(line)) return;
 
                 var input = JSON.parse(line);
                 if (input.type === "genre") {
-                    if ( genres.length > 0 ) {
-                      this.queue( JSON.stringify(genres[genres.length-1]) + "\n" );
+                    if (genres.length > 0) {
+                        this.queue(JSON.stringify(genres[genres.length - 1]) + "\n");
                     }
-                    genres.push({ "name":input.name, "books": [] });
-                } else if ( input.type === "book" ) {
-                    genres[genres.length-1].books.push(input.name);
+                    genres.push({
+                        "name": input.name,
+                        "books": []
+                    });
+                } else if (input.type === "book") {
+                    genres[genres.length - 1].books.push(input.name);
                 }
-              }, function(){
-                   if (  genres.length > 0 ) {
-                     this.queue( JSON.stringify(genres[genres.length-1]) + "\n" );
-                   } 
-                   this.queue(null);
-                 } ),
-              zlib.createGzip()
+            }, function() {
+                if (genres.length > 0) {
+                    this.queue(JSON.stringify(genres[genres.length - 1]) + "\n");
+                }
+                this.queue(null);
+            }),
+            zlib.createGzip()
         );
     };
 
--- line-splitter.js
+++ line-splitter.js
@@ -1,44 +1,30 @@
 (function() {
 
-  'use strict';
-
-/*
- *
-  The argument given to you from `process.argv[2]` will be a path to a file.
-
-  Read this file and split it by newline characters ('\n'). You should log
-  one Buffer per line.
-
-  Bonus points if you never use `.toString()`.
-
-      Buffers have a `.slice` method that can be used to grab a sub-selection as a view (no memcpy).
-
-      Extra bonus points if you avoid using fs.readFileSync.
-*/
+    'use strict';
 
     var fs = require('fs');
     var pathToFile = process.argv[2];
-    fs.readFile(pathToFile, function(err,data) {
-      if (err) {
-          throw err;
-      }
-
-      var buffers = [];
-      var i = 0;
-      var last_slice = 0;
-      for ( ; i < data.length; i++ ) {
-        if ( data[i] === 10 && last_slice != i ) {
-          buffers.push( data.slice( last_slice, i ) );
-          last_slice = i+1; 
-
+    fs.readFile(pathToFile, function(err, data) {
+        if (err) {
+            throw err;
         }
-      }
-      
-      buffers.push( data.slice( last_slice, data.length ) );
-      
-      buffers.forEach(function(buffer) {
-          console.log(buffer);
-      });
+
+        var buffers = [];
+        var i = 0;
+        var last_slice = 0;
+        for (; i < data.length; i++) {
+            if (data[i] === 10 && last_slice != i) {
+                buffers.push(data.slice(last_slice, i));
+                last_slice = i + 1;
+
+            }
+        }
+
+        buffers.push(data.slice(last_slice, data.length));
+
+        buffers.forEach(function(buffer) {
+            console.log(buffer);
+        });
 
     });
 
